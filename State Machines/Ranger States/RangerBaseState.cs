dd9eaa7a47ec08b9a472b908ab84dbd7296ca91a
Mcfarry <mcfarry09@gmail.com>
2026-09-07 12:27:35 +0100

    Delete State Machines directory

--- State Machines/Ranger States/RangerBaseState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public abstract class RangerBaseState
-{
-    public abstract void EnterState(RangerStateManager ranger);
-    public abstract void UpdateState(RangerStateManager ranger);
-}
